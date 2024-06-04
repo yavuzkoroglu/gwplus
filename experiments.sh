4f810094d10abce8d2ef3edb49745ad203aa4c25
yavuzkoroglu <yavuz.koroglu@boun.edu.tr>
2024-06-04 09:50:55 +0200

    on 04.06.24 @ 09:50

--- experiments.sh
+++ experiments.sh
@@ -4,56 +4,83 @@ GW_METHODS="random quick_random"
 GW_COVERAGES="vertex_coverage edge_coverage"
 GW_RATIOS="100 90 80 70 60 50"
 GW_SEEDS="172062 512956 210987 772120 832513 76219 132921 114702 805553 432487 971148 366466 453649 735972 982676 375208 306259 177328 666177 108218 118894 835003 190598 980472 578073 104881 325629 980737 506544 268475"
-GWPLUS_COVERAGES="vertex edge edgepair edgetriple prime1 prime2"
-MODELS="001 002 003 004 005"
+GWPLUS_COVERAGES="vertex edge edgepair"
+MODELS="005"
 NREPEATS=1
 
 NSEEDS=0
-for seed in $GW_SEEDS
+for seed in ${GW_SEEDS}
 do
     ((NSEEDS++))
 done
-echo "# SEEDS   = $NSEEDS"
-echo "# REPEATS = $NREPEATS"
+echo "# SEEDS   = ${NSEEDS}"
+echo "# REPEATS = ${NREPEATS}"
 
-for model in $MODELS
+for model in ${MODELS}
 do
-    echo "Model,Tool,Method,Coverage,Ratio,Seed,i,Test Length (#edges),Test Generation Time (milliseconds)" > $EXPS/$model/results.csv
+    echo "Model,Tool,Method,Target Coverage,Ratio,Seed,i,#symbols,Test Length (#edges),Test Generation Time (milliseconds),Vertex Coverage,Edge Coverage,Edge Pair Coverage,Edge Triple Coverage,Prime Vertex Path Coverage,Prime Edge Path Coverage" > ${EXPS}/${model}/generated/results.csv
 
-    for coverage in $GWPLUS_COVERAGES
+    for coverage in ${GWPLUS_COVERAGES}
     do
-        for i in $(seq $NREPEATS)
+        for ratio in ${GW_RATIOS}
         do
-            start=$(gdate +%s%N)
-            bin/gwplus -i $EXPS/$model/m.json -c $coverage -t $EXPS/$model/tests/gwplus_$coverage.json
-            len=$(java -jar graphwalker-cli-4.3.2.jar offline -m "$EXPS/$model/tests/gwplus_$coverage.json" "predefined_path(predefined_path)" | wc -l)
-            end=$(gdate +%s%N)
-            ((len=len/2 - 1))
-            tm=$(($end-$start))
-            ((tm=tm/1000000))
+            for i in $(seq ${NREPEATS})
+            do
+                start=$(gdate +%s%N)
+                bin/gwplus -i ${EXPS}/${model}/m.json -c ${coverage} -t ${EXPS}/${model}/generated/gwplus_${coverage}_${ratio}_${i}.json ${ratio}
+                java -jar graphwalker-cli-4.3.2.jar offline -o -m "${EXPS}/${model}/generated/gwplus_${coverage}_${ratio}_${i}.json" "predefined_path(predefined_path)" | jq -r '.currentElementID' > ${EXPS}/${model}/generated/gwplus_${coverage}_${ratio}_${i}.txt
+                end=$(gdate +%s%N)
 
-            echo "$model,GWPlus,NA,$coverage,100,0,$i,$len,$tm" >> $EXPS/$model/results.csv
+                len=$(cat ${EXPS}/${model}/generated/gwplus_${coverage}_${ratio}_${i}.txt | wc -l)
+
+                vcov=$(bin/gwplus -i ${EXPS}/${model}/m.json -c vertex -m ${EXPS}/${model}/generated/gwplus_${coverage}_${ratio}_${i}.txt | grep '%' | tr -d '%')
+                ecov=$(bin/gwplus -i ${EXPS}/${model}/m.json -c edge -m ${EXPS}/${model}/generated/gwplus_${coverage}_${ratio}_${i}.txt | grep '%' | tr -d '%')
+                epc=$(bin/gwplus -i ${EXPS}/${model}/m.json -c edgepair -m ${EXPS}/${model}/generated/gwplus_${coverage}_${ratio}_${i}.txt | grep '%' | tr -d '%')
+                etc=$(bin/gwplus -i ${EXPS}/${model}/m.json -c edgetriple -m ${EXPS}/${model}/generated/gwplus_${coverage}_${ratio}_${i}.txt | grep '%' | tr -d '%')
+                pvp=$(bin/gwplus -i ${EXPS}/${model}/m.json -c prime1 -m ${EXPS}/${model}/generated/gwplus_${coverage}_${ratio}_${i}.txt | grep '%' | tr -d '%')
+                pep=$(bin/gwplus -i ${EXPS}/${model}/m.json -c prime2 -m ${EXPS}/${model}/generated/gwplus_${coverage}_${ratio}_${i}.txt | grep '%' | tr -d '%')
+
+                ((symbols=len))
+                ((len=len/2))
+
+                tm=$((${end}-${start}))
+                ((tm=tm/1000000))
+
+                echo "${model},GWPlus,NA,${coverage},${ratio},NA,${i},${symbols},${len},${tm},${vcov},${ecov},${epc},${etc},${pvp},${pep}" >> ${EXPS}/${model}/generated/results.csv
+            done
         done
     done
 
-    for method in $GW_METHODS
+    for method in ${GW_METHODS}
     do
-        for coverage in $GW_COVERAGES
+        for coverage in ${GW_COVERAGES}
         do
-            for ratio in $GW_RATIOS
+            for ratio in ${GW_RATIOS}
             do
-                for seed in $GW_SEEDS
+                for seed in ${GW_SEEDS}
                 do
-                    for i in $(seq $NREPEATS)
+                    for i in $(seq ${NREPEATS})
                     do
                         start=$(gdate +%s%N)
-                        len=$(java -jar graphwalker-cli-4.3.2.jar offline -d $seed -m "$EXPS/$model/m.json" "$method(${coverage}($ratio))" | wc -l)
+                        java -jar graphwalker-cli-4.3.2.jar offline -o -d ${seed} -m "${EXPS}/${model}/m.json" "${method}(${coverage}(${ratio}))" | jq -r '.currentElementID' > ${EXPS}/${model}/generated/graphwalker_${method}_${coverage}_${ratio}_${seed}_${i}.txt
                         end=$(gdate +%s%N)
 
+                        len=$(cat ${EXPS}/${model}/generated/graphwalker_${method}_${coverage}_${ratio}_${seed}_${i}.txt | wc -l)
+
+                        vcov=$(bin/gwplus -i ${EXPS}/${model}/m.json -c vertex -m ${EXPS}/${model}/generated/graphwalker_${method}_${coverage}_${ratio}_${seed}_${i}.txt | grep '%' | tr -d '%')
+                        ecov=$(bin/gwplus -i ${EXPS}/${model}/m.json -c edge -m ${EXPS}/${model}/generated/graphwalker_${method}_${coverage}_${ratio}_${seed}_${i}.txt | grep '%' | tr -d '%')
+                        epc=$(bin/gwplus -i ${EXPS}/${model}/m.json -c edgepair -m ${EXPS}/${model}/generated/graphwalker_${method}_${coverage}_${ratio}_${seed}_${i}.txt | grep '%' | tr -d '%')
+                        etc=$(bin/gwplus -i ${EXPS}/${model}/m.json -c edgetriple -m ${EXPS}/${model}/generated/graphwalker_${method}_${coverage}_${ratio}_${seed}_${i}.txt | grep '%' | tr -d '%')
+                        pvp=$(bin/gwplus -i ${EXPS}/${model}/m.json -c prime1 -m ${EXPS}/${model}/generated/graphwalker_${method}_${coverage}_${ratio}_${seed}_${i}.txt | grep '%' | tr -d '%')
+                        pep=$(bin/gwplus -i ${EXPS}/${model}/m.json -c prime2 -m ${EXPS}/${model}/generated/graphwalker_${method}_${coverage}_${ratio}_${seed}_${i}.txt | grep '%' | tr -d '%')
+
+                        ((symbols=len))
                         ((len=len/2))
-                        tm=$(($end-$start))
+
+                        tm=$((${end}-${start}))
                         ((tm=tm/1000000))
-                        echo "$model,GraphWalker,$method,$coverage,$ratio,$seed,$i,$len,$tm" >> $EXPS/$model/results.csv
+
+                        echo "${model},GraphWalker,${method},${coverage},${ratio},${seed},${i},${symbols},${len},${tm},${vcov},${ecov},${epc},${etc},${pvp},${pep}" >> ${EXPS}/${model}/generated/results.csv
                     done
                 done
             done
--- generateTests.sh
+++ generateTests.sh
@@ -4,33 +4,40 @@ GW_METHODS="random quick_random"
 GW_COVERAGES="vertex_coverage edge_coverage"
 GW_RATIOS="100 90 80 70 60 50"
 GW_SEEDS="172062 512956 210987 772120 832513 76219 132921 114702 805553 432487 971148 366466 453649 735972 982676 375208 306259 177328 666177 108218 118894 835003 190598 980472 578073 104881 325629 980737 506544 268475"
-GWPLUS_COVERAGES="vertex edge edgepair edgetriple prime1 prime2"
-MODELS="001 002 003 004 005"
+GWPLUS_COVERAGES="vertex edge edgepair"
+MODELS="005"
 
 NSEEDS=0
-for seed in $GW_SEEDS
+for seed in ${GW_SEEDS}
 do
     ((NSEEDS++))
 done
-echo "# SEEDS = $NSEEDS"
+echo "# SEEDS = ${NSEEDS}"
 
-for model in $MODELS
+for model in ${MODELS}
 do
-    for coverage in $GWPLUS_COVERAGES
+    for coverage in ${GWPLUS_COVERAGES}
     do
-        bin/gwplus -i $EXPS/$model/m.json -c $coverage -t $EXPS/$model/tests/gwplus_$coverage.json
-        java -jar graphwalker-cli-4.3.2.jar offline -m "$EXPS/$model/tests/gwplus_$coverage.json" "predefined_path(predefined_path)" > $EXPS/$model/tests/gwplus_$coverage.txt
+        for ratio in ${GW_RATIOS}
+        do
+            echo "bin/gwplus -i ${EXPS}/${model}/m.json -c ${coverage} -t ${EXPS}/${model}/generated/gwplus_${coverage}_${ratio}.json ${ratio}"
+            bin/gwplus -i ${EXPS}/${model}/m.json -c ${coverage} -t ${EXPS}/${model}/generated/gwplus_${coverage}_${ratio}.json ${ratio}
+
+            echo "java -jar graphwalker-cli-4.3.2.jar offline -o -m \"${EXPS}/${model}/generated/gwplus_${coverage}_${ratio}.json\" \"predefined_path(predefined_path)\" | jq -r '.currentElementID' > ${EXPS}/${model}/generated/gwplus_${coverage}_${ratio}.txt"
+            java -jar graphwalker-cli-4.3.2.jar offline -o -m "${EXPS}/${model}/generated/gwplus_${coverage}_${ratio}.json" "predefined_path(predefined_path)" | jq -r '.currentElementID' > ${EXPS}/${model}/generated/gwplus_${coverage}_${ratio}.txt
+        done
     done
 
-    for method in $GW_METHODS
+    for method in ${GW_METHODS}
     do
-        for coverage in $GW_COVERAGES
+        for coverage in ${GW_COVERAGES}
         do
-            for ratio in $GW_RATIOS
+            for ratio in ${GW_RATIOS}
             do
-                for seed in $GW_SEEDS
+                for seed in ${GW_SEEDS}
                 do
-                    java -jar graphwalker-cli-4.3.2.jar offline -o -d $seed -m "$EXPS/$model/m.json" "$method(${coverage}($ratio))" | jq -r '.currentElementID' > ${EXPS}/${model}/tests/graphwalker_${method}_${coverage}_${ratio}_${seed}.txt
+                    echo "java -jar graphwalker-cli-4.3.2.jar offline -o -d ${seed} -m \"${EXPS}/${model}/m.json\" \"${method}(${coverage}($ratio))\" | jq -r '.currentElementID' > ${EXPS}/${model}/generated/graphwalker_${method}_${coverage}_${ratio}_${seed}.txt"
+                    java -jar graphwalker-cli-4.3.2.jar offline -o -d ${seed} -m "${EXPS}/${model}/m.json" "${method}(${coverage}($ratio))" | jq -r '.currentElementID' > ${EXPS}/${model}/generated/graphwalker_${method}_${coverage}_${ratio}_${seed}.txt
                 done
             done
         done
