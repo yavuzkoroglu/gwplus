0572a2aa467cf382bd64a26310aafbf6041092d2
yavuzkoroglu <yavuz.koroglu@boun.edu.tr>
2024-07-23 20:53:27 +0300

    on 23.07.24 @ 20:53

--- rq2_raw.sh
+++ rq2_raw.sh
@@ -2,11 +2,16 @@
 EXPS="exps"
 GW_METHODS="random quick_random"
 GW_COVERAGES="vertex_coverage edge_coverage"
-GW_RATIOS="100 90 80 70 60 50"
+GW_RATIOS="100"
 GW_SEEDS="172062 512956 210987 772120 832513 76219 132921 114702 805553 432487 971148 366466 453649 735972 982676 375208 306259 177328 666177 108218 118894 835003 190598 980472 578073 104881 325629 980737 506544 268475"
+GWPLUS_METHODS="gwplus dj"
 GWPLUS_COVERAGES="vertex edge edgepair"
-ALL_COVERAGES="vertex edge edgepair edgetriple prime1 prime2"
-MODELS="005"
+ALL_COVERAGES="vertex edge edgepair edgetriple"
+#ALL_COVERAGES="prime1"
+#ALL_COVERAGES="prime2"
+MODELS="002 003 004 005"
+#MODELS="002 003 005"
+#MODELS="002"
 NREPEATS=1
 
 NSEEDS=0
@@ -18,14 +23,17 @@ echo "# SEEDS = ${NSEEDS}"
 
 for model in ${MODELS}
 do
-    for targetcoverage in ${GWPLUS_COVERAGES}
+    for method in ${GWPLUS_METHODS}
     do
-        for ratio in ${GW_RATIOS}
+        for targetcoverage in ${GWPLUS_COVERAGES}
         do
-            for coverage in ${ALL_COVERAGES}
+            for ratio in ${GW_RATIOS}
             do
-                bin/gwplus -i ${EXPS}/${model}/m.json -c ${coverage} -m ${EXPS}/${model}/generated/gwplus_${targetcoverage}_${ratio}.txt > ${EXPS}/${model}/coverages/gwplus_${targetcoverage}_${ratio}_${coverage}.txt
-                echo "bin/gwplus -i ${EXPS}/${model}/m.json -c ${coverage} -m ${EXPS}/${model}/generated/gwplus_${targetcoverage}_${ratio}.txt > ${EXPS}/${model}/coverages/gwplus_${targetcoverage}_${ratio}_${coverage}.txt"
+                for coverage in ${ALL_COVERAGES}
+                do
+                    bin/gwplus -i ${EXPS}/${model}/m.json -c ${coverage} -m ${EXPS}/${model}/generated/gwplus_${method}_${targetcoverage}_${ratio}*.txt > ${EXPS}/${model}/coverages/gwplus_${method}_${targetcoverage}_${ratio}_${coverage}.txt
+                    echo "bin/gwplus -i ${EXPS}/${model}/m.json -c ${coverage} -m ${EXPS}/${model}/generated/gwplus_${method}_${targetcoverage}_${ratio}*.txt > ${EXPS}/${model}/coverages/gwplus_${method}_${targetcoverage}_${ratio}_${coverage}.txt"
+                done
             done
         done
     done
